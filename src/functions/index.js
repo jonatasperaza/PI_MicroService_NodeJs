42be4bf73f56184e71b4f7d2e37ddaa7914c9397
Caussz <cczcaua@gmail.com>
2024-02-11 12:02:44 -0300

    REFACTOR: Mudancas de lint #4

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,7 +1,9 @@
-const loginSigaa = require('./loginSigaa');
 const initBrowser = require('./initBrowser');
+const loginSigaa = require('./loginSigaa');
+const sigaaMobileOptions = require('./sigaaMobileOptions');
 
 module.exports = {
   loginSigaa,
   initBrowser,
+  sigaaMobileOptions,
 };
