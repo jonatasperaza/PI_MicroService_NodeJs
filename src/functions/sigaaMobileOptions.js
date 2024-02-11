e39211412568caac235f52d799b1fbc6c790273d
Caussz <cczcaua@gmail.com>
2024-02-11 12:07:23 -0300

    DOCS: Adicionando jsDOCS na funcao de mapeamento das opcoes do sigaa mobile #4

--- src/functions/sigaaMobileOptions.js
+++ src/functions/sigaaMobileOptions.js
@@ -1,10 +1,17 @@
 const loginSigaa = require('./loginSigaa');
 
+/**
+ * Inicializa um navegador Puppeteer.
+ * @param {Object} data - Obejto contendo informacao do usuario
+ * e o estado do navegador.
+ * @return {Object} Um objeto com: O navegador
+ *  e as opcoes do menu no modo Mobile.
+ */
 async function sigaaMobileOptions(data) {
   const {user, pass} = data;
   const page = await loginSigaa(user, pass);
 
-  const sigaaOptions = await page.$$(".ui-link-inherit");
+  const sigaaOptions = await page.$$('.ui-link-inherit');
   const classOption = sigaaOptions[0];
   const gradeOption = sigaaOptions[1];
   const enrollmentOptions = sigaaOptions[2];
@@ -19,8 +26,8 @@ async function sigaaMobileOptions(data) {
     exitSIGAA,
     enrollmentOptions,
     academicTranscriptMobile,
-    suplimentEnrollmentOptions
-  }
+    suplimentEnrollmentOptions,
+  };
 };
 
 module.exports = sigaaMobileOptions;
