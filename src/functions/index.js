32738890b8691a34a8705f681e31f7d2d05cc517
Caussz <cczcaua@gmail.com>
2024-02-25 20:16:51 -0300

    REFACTOR: Corrigindo para adiconar as rotas

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,9 +1,13 @@
 const initBrowser = require('./initBrowser');
 const loginSigaa = require('./loginSigaa');
+const studentGradesOptions = require('./studentGradesOptions');
+const getGradesStudents = require('./getGradesStudents');
 const sigaaMobileOptions = require('./sigaaMobileOptions');
 
 module.exports = {
   loginSigaa,
   initBrowser,
   sigaaMobileOptions,
+  getGradesStudents,
+  studentGradesOptions,
 };
